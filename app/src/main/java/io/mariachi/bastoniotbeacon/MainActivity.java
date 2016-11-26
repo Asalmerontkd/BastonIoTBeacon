7d195a4ec734ba00e41cdcbd7847a7a92b0ea2fc
Asalmerontkd <psrantonio@gmail.com>
2016-11-26 09:26:54 -0600

    Beacon Post Add Azure Callback

--- app/src/main/java/io/mariachi/bastoniotbeacon/MainActivity.java
+++ app/src/main/java/io/mariachi/bastoniotbeacon/MainActivity.java
@@ -10,10 +10,25 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.microsoft.azure.iothub.DeviceClient;
+import com.microsoft.azure.iothub.IotHubClientProtocol;
+import com.microsoft.azure.iothub.IotHubEventCallback;
+import com.microsoft.azure.iothub.IotHubMessageResult;
+import com.microsoft.azure.iothub.IotHubStatusCode;
+import com.microsoft.azure.iothub.Message;
+import com.microsoft.azure.iothub.MessageCallback;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+    String connString = "HostName=BastonInteligente.azure-devices.net;DeviceId=MyNewDevice;SharedAccessKey=idd7meI5BG33lyRWeGzhdgwBShjYRCOLdOWWurZx/Oo=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,17 @@ public class MainActivity extends AppCompatActivity {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         System.out.print("Refreshed token:  "+refreshedToken);
         Log.d("Refreshed token: ", refreshedToken);
+        try {
+            SendMessage();
+        }
+        catch(IOException e1)
+        {
+            System.out.println("Exception while opening IoTHub connection: " + e1.toString());
+        }
+        catch(Exception e2)
+        {
+            System.out.println("Exception while opening IoTHub connection: " + e2.toString());
+        }
     }
 
     @Override
@@ -55,5 +81,189 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void SendMessage() throws URISyntaxException, IOException
+    {
+        // Comment/uncomment from lines below to use HTTPS or MQTT protocol
+        IotHubClientProtocol protocol = IotHubClientProtocol.HTTPS;
+        //  IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
+
+        DeviceClient client = new DeviceClient(connString, protocol);
+
+        try {
+            client.open();
+        }
+        catch(IOException e1)
+        {
+            System.out.println("Exception while opening IoTHub connection: " + e1.toString());
+        }
+        catch(Exception e2)
+        {
+            System.out.println("Exception while opening IoTHub connection: " + e2.toString());
+        }
+
+        for (int i = 0; i < 5; ++i)
+        {
+            String msgStr = "Event Message " + Integer.toString(i);
+            try
+            {
+                Message msg = new Message(msgStr);
+                msg.setProperty("messageCount", Integer.toString(i));
+                System.out.println(msgStr);
+                EventCallback eventCallback = new EventCallback();
+                client.sendEventAsync(msg, eventCallback, i);
+            }
+            catch (Exception e)
+            {
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        client.close();
+    }
+
+    public void btnReceiveOnClick(View v) throws URISyntaxException, IOException {
+        Button button = (Button) v;
+
+        // Comment/uncomment from lines below to use HTTPS or MQTT protocol
+        // IotHubClientProtocol protocol = IotHubClientProtocol.HTTPS;
+        IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
+
+        DeviceClient client = new DeviceClient(connString, protocol);
+
+        if (protocol == IotHubClientProtocol.MQTT)
+        {
+            MessageCallbackMqtt callback = new MessageCallbackMqtt();
+            Counter counter = new Counter(0);
+            client.setMessageCallback(callback, counter);
+        }
+        else
+        {
+            MessageCallback callback = new MessageCallback();
+            Counter counter = new Counter(0);
+            client.setMessageCallback(callback, counter);
+        }
+
+        try {
+            client.open();
+        }
+        catch(IOException e1)
+        {
+            System.out.println("Exception while opening IoTHub connection: " + e1.toString());
+        }
+        catch(Exception e2)
+        {
+            System.out.println("Exception while opening IoTHub connection: " + e2.toString());
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        client.close();
+    }
+
+    // Our MQTT doesn't support abandon/reject, so we will only display the messaged received
+    // from IoTHub and return COMPLETE
+    public class MessageCallbackMqtt implements com.microsoft.azure.iothub.MessageCallback
+    {
+        String mensaje;
+        String contenido;
+
+        public IotHubMessageResult execute(Message msg, Object context)
+        {
+            Counter counter = (Counter) context;
+            mensaje = "Received message " + counter.toString();
+            contenido = " with content: " + new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET);
+
+            System.out.println(
+                    "Received message " + counter.toString()
+                            + " with content: " + new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET));
+
+            Log.d("MyLog", mensaje+contenido);
+
+
+            counter.increment();
+
+            return IotHubMessageResult.COMPLETE;
+        }
+
+    }
+
+    protected static class EventCallback implements IotHubEventCallback {
+        public void execute(IotHubStatusCode status, Object context){
+            Integer i = (Integer) context;
+            System.out.println("IoT Hub responded to message "+i.toString()
+                    + " with status " + status.name());
+        }
+    }
+
+    protected static class MessageCallback implements com.microsoft.azure.iothub.MessageCallback
+    {
+        public IotHubMessageResult execute(Message msg, Object context)
+        {
+            Counter counter = (Counter) context;
+            System.out.println(
+                    "Received message " + counter.toString()
+                            + " with content: " + new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET));
+
+            int switchVal = counter.get() % 3;
+            IotHubMessageResult res;
+            switch (switchVal)
+            {
+                case 0:
+                    res = IotHubMessageResult.COMPLETE;
+                    break;
+                case 1:
+                    res = IotHubMessageResult.ABANDON;
+                    break;
+                case 2:
+                    res = IotHubMessageResult.REJECT;
+                    break;
+                default:
+                    // should never happen.
+                    throw new IllegalStateException("Invalid message result specified.");
+            }
+
+            System.out.println("Responding to message " + counter.toString() + " with " + res.name());
+
+            counter.increment();
+
+            return res;
+        }
+    }
+
+    /** Used as a counter in the message callback. */
+    protected static class Counter
+    {
+        protected int num;
+
+        public Counter(int num)
+        {
+            this.num = num;
+        }
+
+        public int get()
+        {
+            return this.num;
+        }
+
+        public void increment()
+        {
+            this.num++;
+        }
+
+        @Override
+        public String toString()
+        {
+            return Integer.toString(this.num);
+        }
     }
 }
